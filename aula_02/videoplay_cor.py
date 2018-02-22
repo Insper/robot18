57d9db8a5019c08adae0ea862a0e5757dc9d564b
Fabio de Miranda <fmiranda@gmail.com>
2018-02-22 15:45:17 -0300

    versao aula do videoplay
    Former-commit-id: feafa658c53fcff2a9085862f6c58197fd8f4106

--- aula_02/videoplay_cor.py
+++ aula_02/videoplay_cor.py
@@ -4,12 +4,12 @@ from matplotlib import pyplot as plt
 import time
 
 # Para usar o vídeo
-cap = cv2.VideoCapture('hall_box_battery_mp2.mp4')
+#cap = cv2.VideoCapture('hall_box_battery_mp2.mp4')
 
 # As 3 próximas linhas são para usar a webcam
-#cap = cv2.VideoCapture(0)
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 
 def identifica_cor(frame):
@@ -19,34 +19,34 @@ def identifica_cor(frame):
 
     # No OpenCV, o canal H vai de 0 até 179, logo cores similares ao 
     # vermelho puro (H=0) estão entre H=-8 e H=8. 
-    # Precisamos dividir o inRange em duas partes para fazer a detecção 
-    # do vermelho:
+    # Veja se este intervalo de cores está bom
     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     cor_menor = np.array([0, 50, 50])
     cor_maior = np.array([8, 255, 255])
     segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
 
-    cor_menor = np.array([172, 50, 50])
-    cor_maior = np.array([180, 255, 255])
-    segmentado_cor += cv2.inRange(frame_hsv, cor_menor, cor_maior)
+    # Será possível limpar a imagem segmentado_cor? 
+    # Pesquise: https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
 
-
-    # A operação MORPH_CLOSE fecha todos os buracos na máscara menores 
-    # que um quadrado 7x7. É muito útil para juntar vários 
-    # pequenos contornos muito próximos em um só.
-    segmentado_cor = cv2.morphologyEx(segmentado_cor,cv2.MORPH_CLOSE,np.ones((7, 7)))
 
     # Encontramos os contornos na máscara e selecionamos o de maior área
     img_out, contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
     maior_contorno = None
     maior_contorno_area = 0
 
+
+    cv2.drawContours(frame, contornos, -1, [255, 0, 255], 5)
+
+
     for cnt in contornos:
         area = cv2.contourArea(cnt)
+
+
         if area > maior_contorno_area:
             maior_contorno = cnt
             maior_contorno_area = area
+
     
     # Encontramos o centro do contorno fazendo a média de todos seus pontos.
     if not maior_contorno is None :
@@ -58,8 +58,8 @@ def identifica_cor(frame):
     else:
         media = (0, 0)
 
-    cv2.imshow('video', frame)
-    cv2.imshow('seg', segmentado_cor)
+    cv2.imshow('', frame)
+    cv2.imshow('imagem in_range', segmentado_cor)
     cv2.waitKey(1)
 
     centro = (frame.shape[0]//2, frame.shape[1]//2)
@@ -69,18 +69,18 @@ def identifica_cor(frame):
 
 while(True):
     # Capture frame-by-frame
-    print("New frame")
+    print("Novo frame")
     ret, frame = cap.read()
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img = frame
+
+
+    img = frame.copy()
 
     media, centro = identifica_cor(img)
 
     #More drawing functions @ http://docs.opencv.org/2.4/modules/core/doc/drawing_functions.html
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('original',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
