3fd921c9b2cf69600942edfd624ecbfd0571e165
Fabio de Miranda <fmiranda@gmail.com>
2018-03-29 11:04:37 -0300

    atualizacao do cor.py com print de delay

--- ros/exemplos_python/scripts/cor.py
+++ ros/exemplos_python/scripts/cor.py
@@ -14,71 +14,21 @@ from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+import cormodule
+
 
 bridge = CvBridge()
 
 cv_image = None
 media = []
 centro = []
-atraso = 1.5
+atraso = 1.5E9 # 1 segundo e meio. Em nanossegundos
 
-check_delay = False # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados
+area = 0.0 # Variavel com a area do maior contorno
 
-def identifica_cor(frame):
-	'''
-	Segmenta o maior objeto cuja cor é parecida com cor_h (HUE da cor, no espaço HSV).
-	'''
-
-	# No OpenCV, o canal H vai de 0 até 179, logo cores similares ao 
-	# vermelho puro (H=0) estão entre H=-8 e H=8. 
-	# Precisamos dividir o inRange em duas partes para fazer a detecção 
-	# do vermelho:
-	frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-	cor_menor = np.array([0, 50, 50])
-	cor_maior = np.array([8, 255, 255])
-	segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
-
-	cor_menor = np.array([172, 50, 50])
-	cor_maior = np.array([180, 255, 255])
-	segmentado_cor += cv2.inRange(frame_hsv, cor_menor, cor_maior)
+check_delay = False # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. Descarta imagens que chegam atrasadas demais
 
 
-	# A operação MORPH_CLOSE fecha todos os buracos na máscara menores 
-	# que um quadrado 7x7. É muito útil para juntar vários 
-	# pequenos contornos muito próximos em um só.
-	segmentado_cor = cv2.morphologyEx(segmentado_cor,cv2.MORPH_CLOSE,np.ones((7, 7)))
-
-	# Encontramos os contornos na máscara e selecionamos o de maior área
-	#contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)	
-	img_out, contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
-
-	maior_contorno = None
-	maior_contorno_area = 0
-
-	for cnt in contornos:
-	    area = cv2.contourArea(cnt)
-	    if area > maior_contorno_area:
-	        maior_contorno = cnt
-	        maior_contorno_area = area
-
-	# Encontramos o centro do contorno fazendo a média de todos seus pontos.
-	if not maior_contorno is None :
-	    cv2.drawContours(frame, [maior_contorno], -1, [0, 0, 255], 5)
-	    maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
-	    media = maior_contorno.mean(axis=0)
-	    media = media.astype(np.int32)
-	    cv2.circle(frame, tuple(media), 5, [0, 255, 0])
-	else:
-	    media = (0, 0)
-
-	cv2.imshow('video', frame)
-	cv2.imshow('seg', segmentado_cor)
-	cv2.waitKey(1)
-
-	centro = (frame.shape[0]//2, frame.shape[1]//2)
-
-	return media, centro
 
 
 
@@ -91,13 +41,15 @@ def roda_todo_frame(imagem):
 	now = rospy.get_rostime()
 	imgtime = imagem.header.stamp
 	lag = now-imgtime
-	delay = lag.secs
+	delay = lag.nsecs
+	print("delay ", "{:.3f}".format(delay/1.0E9))
 	if delay > atraso and check_delay==True:
+		print("Descartando por causa do delay do frame:", delay)
 		return 
 	try:
 		antes = time.clock()
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
-		media, centro = identifica_cor(cv_image)
+		media, centro, area =  cormodule.identifica_cor(cv_image)
 		depois = time.clock()
 		cv2.imshow("Camera", cv_image)
 	except CvBridgeError as e:
@@ -108,11 +60,17 @@ def roda_todo_frame(imagem):
 if __name__=="__main__":
 
 	rospy.init_node("cor")
+
 	# Para usar a Raspberry Pi
-	recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=1, buff_size = 2**24)
-	
+	topico_raspberry_camera = "/raspicam_node/image/compressed"
 	# Para usar a webcam 
-	#recebedor = rospy.Subscriber("/cv_camera/image_raw/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
+	topico_webcam = "/cv_camera/image_raw/compressed"
+
+
+	topico_imagem = topico_raspberry_camera
+
+	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+	print("Usando ", topico_imagem)
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
